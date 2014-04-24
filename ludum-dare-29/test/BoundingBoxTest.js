7e21e8a830e775c2e0c25d3d6ef2a429a5d1b5db
Christopher D Canfield <cdcanfield@gmail.com>
2014-04-24 15:16:01 -0400

    .project is now included; Added BoundingBoxTest.js

---
+++ ludum-dare-29/test/BoundingBoxTest.js
@@ -0,0 +1,12 @@
+/**
+ * BoundingBoxTest.js
+ * @author Christopher D. Canfield
+ */
+
+<script type="text/javascript" src="../src/util/BoundingBox.js"></script>
+
+test("intersects", function() {
+	var box1 = new BoundingBox(1, 3, 4, 3, 1, 3);
+	var box2 = new BoundingBox(2, 1, 3, 2, 2, 1);
+	
+});
