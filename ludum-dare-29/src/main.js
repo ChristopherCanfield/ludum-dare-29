1de98545ed81335736a6804e0ef2e3d48f5d7347
Christopher D Canfield <cdcanfield@gmail.com>
2014-04-25 18:46:59 -0400

    Playing around with physi.js

--- ludum-dare-29/src/main.js
+++ ludum-dare-29/src/main.js
@@ -1,117 +1,66 @@
 /**
  * @author Christopher D. Canfield
- * 2013-10-22
- * Updated November 2013
+ * 2014-04-25
  */
 
+ var initialize, render, renderer, scene, camera, box;
 
-/**
- * The OpenGL context.
- */
-var gl;
+initialize = function() {
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.getElementById( 'viewport' ).appendChild( renderer.domElement );
 
-// OpenGL Constants
-var glCOLOR_BUFFER_BIT = true;
-var glDEPTH_BUFFER_BIT = true;
+    scene = new Physijs.Scene({ fixedTimeStep: 1/60});
+    scene.setGravity(new THREE.Vector3(0, -9.8, 0));
 
-var cdc = {
-    /**
-     * Processes camera movements.
-     */
-    camera: null, 
-    
-    /**
-     * The root of the scene graph.
-     */
-    scene: null,
-    
-    /**
-     * Assists with loading textures.
-     */
-    textureManager: null,
-    
-    /**
-     * Processes user input related to the camera.
-     */
-    cameraController: null,
-    
-    /**
-     * Processes user input related to the weapon. 
-     */
-    weaponController: null,
-    
-    /**
-     * Timer used for logic, movement & animation.
-     */
-    timer: null,
-    
-    lastUpdate: 0
+    camera = new THREE.PerspectiveCamera(
+        35,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000
+    );
+    camera.position.set( 60, 50, 60 );
+    camera.lookAt( scene.position );
+    scene.add( camera );
+
+    // Box
+    box = new Physijs.BoxMesh(
+        new THREE.BoxGeometry( 5, 5, 5 ),
+        Physijs.createMaterial(
+	        new THREE.MeshPhongMaterial({ color: 0x888888 }),
+    	    0.8, // friction
+    	    0.4 // restitution
+    	),
+    	1	// weight
+    );
+    box.position.set(0, 20, 0);
+    box.__dirtyPosition = true;
+    scene.add(box);
+
+	var light = new THREE.PointLight(0xffffff, 1, 100);
+	light.position.set(30, 30, 50);
+	scene.add(light);
+	
+	var ground = new Physijs.BoxMesh(
+		new THREE.BoxGeometry(50, 1, 50),
+		new THREE.MeshPhongMaterial({ color: 0x0094ff})
+	);
+	ground.position.set(0, 0, 0);
+	ground.__dirtyPosition = true;
+	scene.add(ground);
+
+    requestAnimationFrame( render );
 };
 
-init();
-render();
-
-function init()
-{
-     'use strict';
-    var glCanvas = document.getElementById("gl-canvas");
+render = function() {
+	// simulate physics.
+    scene.simulate();
     
-    var glParameters = {
-        canvas: glCanvas,
-        alpha: false,
-        antialias: true     // TODO: allow user to turn this on or off.
-    };
-    
-    // Create the webGL render.
-    gl = new THREE.WebGLRenderer(glParameters);
-    gl.setClearColor(Colors.SKY_BLACK); // gl.clearColor(r, g, b);
-    gl.setDepthTest(true); // gl.enable(gl.DEPTH_TEST);
-    
-    cdc.textureManager = new TextureManager(gl);
-    
-    // Create the scene graph root node, and populate the scene.
-    cdc.scene = new GameScene();
-    cdc.scene.createScene();
-    
-    // Create the camera so it can be moved.
-    cdc.camera = new Camera(cdc.scene.getZones(), window, glCanvas.width, glCanvas.height);
-    //cdc.camera.debug = true;
-    cdc.cameraController = new CameraController(glCanvas.width, glCanvas.height, cdc.camera, cdc.scene.getDoors());
-    cdc.weaponController = new WeaponController(cdc.scene.getThreeJsScene(), cdc.camera);
-    
-    cdc.scene.getThreeJsScene().add(cdc.camera.yawObject);
-    
-    cdc.timer = new THREE.Clock();
-    cdc.timer.start();
-    
-    render();
+    // render the scene.
+    renderer.render( scene, camera);
+     
+     box.rotation.x += .05;
+     box.__dirtyRotation = true;
+     
+    requestAnimationFrame( render );
 };
-
-
-/**
- * Draw the scene.
- */
-function render()
-{
-    var deltaTime = cdc.timer.getDelta();
-    cdc.lastUpdate += deltaTime;
-    
-    // process camera movements.
-    cdc.camera.update(deltaTime);
-    
-    // Process logic, particle/object movements and animation updates.
-    cdc.scene.update(deltaTime);
-    
-    cdc.scene.getZones().updateParticles(deltaTime, cdc.camera.getBoundingBox());
-    cdc.weaponController.update(deltaTime);
-    
-    if (cdc.lastUpdate > 0.033)
-    {
-        // Clear & render the scene.
-        gl.clear(glCOLOR_BUFFER_BIT, glDEPTH_BUFFER_BIT); //gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-        gl.render(cdc.scene.getThreeJsScene(), cdc.camera.getThreeJSCamera());
-        cdc.lastUpdate = 0;
-    }
-    
-    window.requestAnimationFrame(render);
-}
