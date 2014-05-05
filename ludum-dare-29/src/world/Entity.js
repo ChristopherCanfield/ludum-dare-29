b8a64b756b1e031b4cf0fdb8fc9afb7f4e0ea15f
Christopher D. Canfield <cdcanfield@gmail.com>
2014-05-05 01:43:44 -0400

    Added javadoc comments to the Entity

--- ludum-dare-29/src/world/Entity.js
+++ ludum-dare-29/src/world/Entity.js
@@ -3,6 +3,10 @@
  * @author Christopher D. Canfield
  */
 
+/**
+ * The base game simulation type. 
+ * @param {Object} mesh
+ */
 function Entity(mesh) {
 	this.mesh = mesh;
 	this.disposed = false;
@@ -17,6 +21,9 @@ function Entity(mesh) {
 	this.observers = [];
 };
 
+/**
+ * Called once per tick. 
+ */
 Entity.prototype.update = function() {
 	var length = this.controllers.length;
 	for (var i = 0; i < length; ++i)
@@ -25,11 +32,20 @@ Entity.prototype.update = function() {
 	}
 };
 
+/**
+ * Adds a controller to this entity.
+ * @param {Controller} controller the controller to add.
+ */
 Entity.prototype.addController = function(controller) {
 	controller.setEntity(this);
 	this.controllers.push(controller);
 };
 
+/**
+ * Gets the specified controller type from the entity. 
+ * @param {String} controllerClass the string value of the controller class type, 
+ * or null if not found.
+ */
 Entity.prototype.getController = function(controllerClass) {
 	for (var i = 0; i < this.controllers.length; ++i)
 	{
@@ -40,10 +56,19 @@ Entity.prototype.getController = function(controllerClass) {
 	}
 };
 
+/**
+ *  Adds a component to the entity.
+ * @param {Component} component the component to add.
+ */
 Entity.prototype.addComponent = function(component) {
 	this.components[component.getClass()] = component;
 };
 
+/**
+ *  Gets the specified component type from the entity. 
+ * @param {String} componentClass the string value of the controller class type,
+ * or null if not found.
+ */
 Entity.prototype.getComponent = function(componentClass) {
 	return this.components[componentClass];
 };
@@ -101,10 +126,16 @@ Entity.prototype.accelerate = function(force) {
 		velocity.z + force.z));
 };
 
+/**
+ * Disposes the entity, which flags the entity for removal from the world. 
+ */
 Entity.prototype.dispose = function() {
 	this.disposed = true;
 };
 
+/**
+ * Returns true if the entity is disposed, or false otherwise. 
+ */
 Entity.prototype.isDisposed = function() {
 	return this.disposed;
 };
